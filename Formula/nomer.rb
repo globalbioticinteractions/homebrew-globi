12418ed9154f18d5c954ddb7d67c6e15da8d91f0
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-07-15 12:58:10 -0500

    add nomer formula

---
+++ Formula/nomer.rb
@@ -0,0 +1,20 @@
+class Nomer < Formula
+  desc "Maps Identifiers and Names to Other IdentNfiers and Names"
+  homepage "https://github.com/bio-guoda/preston"
+  url "https://github.com/globalbioticinteractions/nomer/releases/download/0.6.0/nomer.tar.gz"
+  sha256 "e4db48cf5b7ce6728680d4c812be4c6d3435fde2781f2b3ba89c2c7eb2154e22"
+  license "GPLv3"
+
+  depends_on "openjdk@21"
+
+  def install
+    libexec.install "lib/nomer.jar"
+    bin.write_jar_script libexec/"nomer.jar", "nomer"
+  end
+
+  test do
+    output = pipe_output("#{bin}/nomer append --include-header gbif-parse", "\tHomo sapiens")
+    assert_match "resolvedTaxonRank", output
+    assert_match "SAME_AS", output
+  end
+end
