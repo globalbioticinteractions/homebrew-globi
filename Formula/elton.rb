4448f2764b1c7ff1c5b894df50cb31ceb25569bd
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-07-15 12:15:03 -0500

    add formula for elton v0.16.0

---
+++ Formula/elton.rb
@@ -0,0 +1,19 @@
+class Elton < Formula
+  desc "Access, Review and Index Existing Species Interaction Datasets "
+  homepage "https://github.com/bio-guoda/preston"
+  url "https://github.com/bio-guoda/preston/releases/download/0.16.0/elton.tar.gz"
+  sha256 "21b084bf9f67f75d37a22298583fc8d72023bbee5584b2652d60ab59b8bbe47a"
+  license "GPLv3"
+
+  depends_on "openjdk@21"
+
+  def install
+    libexec.install "lib/elton.jar"
+    bin.write_jar_script libexec/"elton.jar", "elton"
+  end
+
+  test do
+    output = shell_output("#{bin}/elton", "interactions", "globalbalbioticinteractions/template-dataset")
+    assert_match "sourceTaxonName", output
+  end
+end
