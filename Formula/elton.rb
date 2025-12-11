8af308b61ab6b2d3ae5caaacc3a68125b20e067c
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2025-12-11 10:45:51 -0600

    upgrade to elton v0.16.1

--- Formula/elton.rb
+++ Formula/elton.rb
@@ -1,8 +1,8 @@
 class Elton < Formula
   desc "Access, Review and Index Existing Species Interaction Datasets "
-  homepage "https://github.com/bio-guoda/preston"
-  url "https://github.com/globalbioticinteractions/elton/releases/download/0.16.0/elton.tar.gz"
-  sha256 "21b084bf9f67f75d37a22298583fc8d72023bbee5584b2652d60ab59b8bbe47a"
+  homepage "https://github.com/globalbioticinteractions/elton"
+  url "https://github.com/globalbioticinteractions/elton/releases/download/0.16.1/elton.tar.gz"
+  sha256 "fd51d9bc86d93c79f366ad5e1cf887c1f4c393506eef74fdb06679f6e7cb0251"
   license "GPLv3"
 
   depends_on "openjdk@21"
